29e9fb429c86f1cc9476a6fca68b0cf8bea9e09d
navalkishor768 <navalkishor768@gmail.com>
2023-10-29 23:39:46 +0530

    Hello World Successful printed on OLED

--- main.c
+++ main.c
@@ -11,6 +11,13 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include "font.c"
+#include "i2cnaval.c"
+#include "i2c_oled_SSD1306.c"
+
+/*// OLED Start Here //*/
+
+/*// OLED END Here //*/
 
 
 
@@ -19,12 +26,24 @@ int main(void)
     /* Replace with your application code */
 	DDRD = 0xFF;
 	PORTD = 0x00;
+	//i2c_init();
+	lcd_init(LCD_DISP_ON);
+	_delay_ms(100);
+	lcd_puts("Hello World");  // put string from RAM to display (TEXTMODE) or buffer (GRAPHICMODE)
+	lcd_gotoxy(0,2);          // set cursor to first column at line 3
+	lcd_puts_p(PSTR("String from flash"));  // puts string form fl
+	
     while (1) 
     {
 		PORTD = 0x00;
-		_delay_ms(500);
+		_delay_ms(1000);
 		PORTD = 0xff;
-		_delay_ms(500);
+		//i2c_write('A');
+		_delay_ms(1000);
+		
+		
+		
+	
 		
     }
 }
